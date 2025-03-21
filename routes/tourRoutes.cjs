c8ecd266df51a3682dd05c1dba61f573758ba7e5
Kyle Law <dev@anzenketh.net>
2025-03-21 12:31:46 -0600

    Remove code used for testing without DB

--- routes/tourRoutes.cjs
+++ routes/tourRoutes.cjs
@@ -4,17 +4,14 @@ const tourController = require('../controller/toursController.cjs');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 // router.param('id', (req, res, next, val) => {
 //   console.log(`Tour id is: ${val}`);
 //   next();
 // });
 
-router
-  .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
 router
   .route('/:id')
